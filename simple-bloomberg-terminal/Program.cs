40025ff1b400853762cd150af2985e6ab6819607
Luka Osojnik <lukaosojnikinfo@gmail.com>
2026-06-28 18:04:55 +0200

    Pushing DOckerfile for railway sync

--- simple-bloomberg-terminal/Program.cs
+++ simple-bloomberg-terminal/Program.cs
@@ -242,12 +242,11 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
 
 var supportedCultures = new[]
 {
